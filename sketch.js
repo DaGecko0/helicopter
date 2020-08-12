663dc178176672f1e0aac0a200833003269efe36
DaGecko0 <68522294+DaGecko0@users.noreply.github.com>
2020-08-12 12:16:00 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var helicopterIMG, helicopterSprite;
+var ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+
+	
+
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+ 
+ 
+  if (keyCode === DOWN_ARROW) {
+ var box =	new box(helicopter.x, helicopter.y, width, height);
+ box.display();   
+	} 
+	
+  drawSprites();
+ 
+}
+
+
+
+
+
